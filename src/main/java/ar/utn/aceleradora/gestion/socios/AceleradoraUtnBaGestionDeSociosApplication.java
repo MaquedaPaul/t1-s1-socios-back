f186ebdc9ece816732efb6d81cbd406b3a60d3db
OviedoFrancoAlexander <64283850+OviedoFranc@users.noreply.github.com>
2023-09-12 15:35:43 -0300

    Commit Inicial del proyecto Aceleradora parte BackEnd

---
+++ src/main/java/ar/utn/aceleradora/gestion/socios/AceleradoraUtnBaGestionDeSociosApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.aceleradora.gestion.socios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceleradoraUtnBaGestionDeSociosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceleradoraUtnBaGestionDeSociosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/utn/aceleradora/gestion/socios/AceleradoraUtnBaGestionDeSociosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.utn.aceleradora.gestion.socios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceleradoraUtnBaGestionDeSociosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
