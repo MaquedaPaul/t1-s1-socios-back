31fedd35673076865b9f4522c827557835308c67
OviedoFrancoAlexander <64283850+OviedoFranc@users.noreply.github.com>
2023-09-27 01:01:04 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Función para convertir rutas de Windows a formato UNIX
+convertir_ruta() {
+    local ruta_original="$1"
+    local ruta_convertida=$(echo "$ruta_original" | sed 's/\\/\//g' | sed 's/C:/\/c/')
+    echo "$ruta_convertida"
+}
+
 # Descargo el script wait-for-it
 echo "Descargando el script wait-for-it.sh..."
 curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
@@ -10,15 +17,21 @@ if [ $? -ne 0 ]; then
 fi
 
 read -p "Introduce la ruta donde quieres guardar las imágenes: " ruta_imagenes
-# si el directorio no esta yo lo creo para que no hay error
+
+# Convertir la ruta introducida a formato compatible con Docker
+ruta_imagenes=$(convertir_ruta "$ruta_imagenes")
+
+# Exportar la variable para que esté disponible para subprocesos
+export PATH_IMAGENES="$ruta_imagenes"
+
+# Si el directorio no está, lo creo para que no haya error
 [ ! -d "$ruta_imagenes" ] && mkdir -p "$ruta_imagenes"
 
 chmod +x wait-for-it.sh
 
 # Uso Docker Compose para construir y ejecutar los servicios
 echo "Usando Docker Compose para construir y ejecutar los servicios..."
-PATH_IMAGENES=$ruta_imagenes docker-compose up --build -d
-
+docker-compose up --build -d
 
 if [ $? -ne 0 ]; then
   echo "Error al usar Docker Compose."
