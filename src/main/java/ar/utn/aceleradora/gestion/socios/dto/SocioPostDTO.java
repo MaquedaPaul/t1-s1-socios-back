baca3a5072a9e562b4dbe1ac95ee8abd744f1675
Carrizo-Gonzalo <gocarrizo@frba.utn.edu.ar>
2023-09-28 16:43:58 -0300

    Cambio apellidoPresidente por nombrePresidente

--- src/main/java/ar/utn/aceleradora/gestion/socios/dto/SocioPostDTO.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/dto/SocioPostDTO.java
@@ -1,18 +1,16 @@
 package ar.utn.aceleradora.gestion.socios.dto;
-import ar.utn.aceleradora.gestion.socios.modelos.departamento.Categoria;
 import ar.utn.aceleradora.gestion.socios.modelos.empresa.TipoSocio;
 import ar.utn.aceleradora.gestion.socios.modelos.ubicacion.Ubicacion;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
 @Getter
 @Setter
 public class SocioPostDTO{
   private String nombre;
-  private String apellidoPresidente;
+  private String nombrePresidente;
   private TipoSocio tipoSocio;
   private String mail;
   private String telefono;
   private Ubicacion ubicacion;
-  }
+}
