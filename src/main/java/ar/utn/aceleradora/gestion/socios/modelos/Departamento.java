2547ba33b03854db22fa71e8c8f08c91d226242a
fedealvarenga <82116673+fedealvarenga@users.noreply.github.com>
2023-10-20 12:47:10 -0300

    agrego import en Departamento.java

--- src/main/java/ar/utn/aceleradora/gestion/socios/modelos/Departamento.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/modelos/Departamento.java
@@ -1,6 +1,7 @@
 package ar.utn.aceleradora.gestion.socios.modelos;
 
 import ar.utn.aceleradora.gestion.socios.converters.LocalDateTimeAttributeConverter;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import ar.utn.aceleradora.gestion.socios.modelos.empresa.Socio;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
