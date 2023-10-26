7b60b0d26793a9476674946a538601fe79fc3141
Rodrigo Gutson <82126714+RodrigoGutson@users.noreply.github.com>
2023-10-26 14:35:10 -0300

    agrego clase Inscripto, tipoEstadoInscripto, EstadoInscripto

---
+++ src/main/java/ar/utn/aceleradora/gestion/socios/modelos/eventos/inscriptos/EstadoInscripto.java
@@ -0,0 +1,9 @@
+package ar.utn.aceleradora.gestion.socios.modelos.eventos.inscriptos;
+
+import java.time.LocalDateTime;
+
+public class EstadoInscripto {
+    private TipoEstadoInscripto estado;
+    private LocalDateTime fechaYHora;
+    private String motivo;
+}
