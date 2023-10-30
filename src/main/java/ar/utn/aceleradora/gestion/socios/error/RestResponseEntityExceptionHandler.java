b11a18ab0f4f4e2e54d52f6c5811c4a8673c3976
MaquedaPaul <maquedapablodavid@gmail.com>
2023-10-29 23:03:46 -0300

    Minor fixes -Uso de not nulls -Comento asignación no definida

--- src/main/java/ar/utn/aceleradora/gestion/socios/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/error/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package ar.utn.aceleradora.gestion.socios.error;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -26,11 +27,9 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, @NotNull HttpHeaders headers, @NotNull HttpStatusCode status, @NotNull WebRequest request) {
         Map<String,Object> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->{
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
