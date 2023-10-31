610dbfbf75536c756885cbae0899477d05b9ed4c
Rodrigo Gutson <82126714+RodrigoGutson@users.noreply.github.com>
2023-10-31 15:02:47 -0300

    Update EventoCreateDTO.java

--- src/main/java/ar/utn/aceleradora/gestion/socios/dto/EventoCreateDTO.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/dto/EventoCreateDTO.java
@@ -11,7 +11,7 @@ public class EventoCreateDTO {
     private String descripcion;
     private String fechaComienzo;
     private String fechaFin;
-    private TipoModalidad modalidad;
+    private Integer modalidad;
     private String direccion;
     private String piso;
     private String departamento;
