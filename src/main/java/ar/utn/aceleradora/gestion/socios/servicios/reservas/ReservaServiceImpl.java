51cefb8e49f76f65ff05219edbbbb3157bfdf679
MaquedaPaul <maquedapablodavid@gmail.com>
2023-11-08 18:35:11 -0300

    Corrijo error y añado nueva clase

--- src/main/java/ar/utn/aceleradora/gestion/socios/servicios/reservas/ReservaServiceImpl.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/servicios/reservas/ReservaServiceImpl.java
@@ -1,4 +1,7 @@
 package ar.utn.aceleradora.gestion.socios.servicios.reservas;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservaServiceImpl implements ReservaService{
 }
