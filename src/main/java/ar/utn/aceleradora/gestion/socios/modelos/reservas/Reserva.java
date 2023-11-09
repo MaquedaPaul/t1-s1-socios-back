36897a6dd46128dd757edf40f87c442127326c9b
MaquedaPaul <maquedapablodavid@gmail.com>
2023-11-09 17:04:17 -0300

    Las nuevas clases ahora son entidades en nuestra db -Se añaden ids a las entidades

--- src/main/java/ar/utn/aceleradora/gestion/socios/modelos/reservas/Reserva.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/modelos/reservas/Reserva.java
@@ -1,4 +1,13 @@
 package ar.utn.aceleradora.gestion.socios.modelos.reservas;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "reservas")
+@Getter
 public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
