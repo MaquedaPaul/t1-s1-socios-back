3f40cf05fd6ece69e1785b8f478e9704db24f242
MaquedaPaul <maquedapablodavid@gmail.com>
2023-11-10 14:07:00 -0300

    Implementación de la generación del código (FALTA) -Falta cambiar el id por el correspondiente -Falta testearlo -Falta implementarlo en el constructor

--- src/main/java/ar/utn/aceleradora/gestion/socios/modelos/reservas/Reserva.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/modelos/reservas/Reserva.java
@@ -1,4 +1,22 @@
 package ar.utn.aceleradora.gestion.socios.modelos.reservas;
 
+import java.security.SecureRandom;
+
 public class Reserva {
+
+
+    private String generarCodigoSeguimiento(){
+        Integer id = 5; //--> reemplazar por getId();
+        String idString = id.toString();
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder codigo = new StringBuilder();
+
+        for (int i = 0; i < 11; i++) {
+            int indice = random.nextInt(caracteres.length());
+            codigo.append(caracteres.charAt(indice));
+        }
+        codigo.append(idString);
+        return codigo.toString();
+    }
 }
