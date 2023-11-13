34b60e5555b3b87bac0cf0c7eba77e28345c8a43
MaquedaPaul <maquedapablodavid@gmail.com>
2023-11-13 16:57:48 -0300

    Implementación get espacios físicos

--- src/main/java/ar/utn/aceleradora/gestion/socios/controladores/departamentos/DepartamentoController.java
+++ src/main/java/ar/utn/aceleradora/gestion/socios/controladores/departamentos/DepartamentoController.java
@@ -8,6 +8,7 @@ import ar.utn.aceleradora.gestion.socios.error.departamentos.DepartamentoNotFoun
 import ar.utn.aceleradora.gestion.socios.modelos.departamentos.Autoridad;
 import ar.utn.aceleradora.gestion.socios.modelos.departamentos.Coordinacion;
 import ar.utn.aceleradora.gestion.socios.modelos.departamentos.Departamento;
+import ar.utn.aceleradora.gestion.socios.modelos.reservas.EspacioFisico;
 import ar.utn.aceleradora.gestion.socios.servicios.departamentos.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -157,6 +158,13 @@ public class DepartamentoController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+    }
+    //Se plantea desde el departamento porque teóricamente pertenecen a los departamentos, pero como actualmente se consideran que t
+    // todos los espacios físicos pertenecen a todos los departamentos no lo implementamos totalmente
+    @GetMapping("/espaciosFisicos")
+    public ResponseEntity<List<EspacioFisico>> obtenerEspaciosFisicos() {
+        List<EspacioFisico> espacioFisicos = departamentoService.obtenerEspaciosFisicos();
+        return new ResponseEntity<>(espacioFisicos, HttpStatus.OK);
     }
 
 
